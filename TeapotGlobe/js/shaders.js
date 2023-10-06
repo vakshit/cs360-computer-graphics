5bd59ec47590347b58cdb83ec833b58390accd21
vakshit <akshitv18@gmail.com>
2023-10-07 02:19:25 +0530

    add cubemap

---
+++ TeapotGlobe/js/shaders.js
@@ -0,0 +1,144 @@
+class Shaders {
+  constructor() {
+    this.skybox = {
+      name: "skybox",
+
+      vertexShaderSource: `
+      attribute vec3 aVertexPosition;
+
+      uniform mat4 uPMatrix;
+      uniform mat4 uMVMatrix;
+
+      varying vec3 vPosition;
+
+      void main(void) {
+      gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
+      vPosition= aVertexPosition;
+      }`,
+
+      fragmentShaderSource: `
+      precision mediump float;
+
+      uniform samplerCube uEnv;
+
+      varying vec3 vPosition;
+
+      void main(void) {
+      gl_FragColor = textureCube(uEnv, normalize(vPosition));
+      }`,
+    };
+
+    this.teapot = {
+      name: "teapot",
+
+      vertexShaderSource: `
+      attribute vec3 aVertexPosition;
+      attribute vec3 aVertexNormal;
+
+      uniform mat4 uPMatrix;
+      uniform mat4 uMVMatrix;
+      uniform mat4 uModelMatrix;
+
+      varying vec3 vModelPosition;
+      varying vec3 vModelNormal;
+      varying vec3 vWorldPosition;
+      varying vec3 vWorldNormal;
+      varying vec3 vVertexColor;
+
+      void main(void) {
+
+      vModelPosition = aVertexPosition;
+      vModelNormal = aVertexNormal;
+
+      vec4 worldPosition = uModelMatrix * vec4(aVertexPosition, 1.0);
+
+      vWorldPosition = vec3(worldPosition);
+      vWorldNormal = normalize(mat3(uModelMatrix) * aVertexNormal);
+      vVertexColor = vec3(0.8,0.8,0.8);
+
+      gl_Position = uPMatrix * uMVMatrix * worldPosition;
+      }`,
+
+      fragmentShaderSource: `
+      precision mediump float;
+
+      const float shininess = 1000.0;
+      const float PI = 3.1415926535897932384626433832795;
+
+      uniform vec3 uViewOrigin;
+      uniform vec3 uLightDirection;
+      uniform vec3 uAmbientLight;
+      uniform vec3 uDiffuseLight;
+      uniform vec3 uSpecularLight;
+
+      uniform samplerCube uEnv;
+      uniform sampler2D uNormalMap;
+
+      uniform float uNormalEnable;
+      uniform float uReflectionEnable;
+      uniform float uBumpiness;
+
+      varying vec3 vModelPosition;
+      varying vec3 vModelNormal;
+      varying vec3 vWorldPosition;
+      varying vec3 vWorldNormal;
+      varying vec3 vVertexColor;
+
+      void main(void) {
+
+      vec3 worldPosition = vWorldPosition;
+      vec3 worldNormal = normalize(vWorldNormal);
+      vec3 modelPosition = vModelPosition;
+      // Adjust to actual center of teapot.
+      modelPosition.y -= 1.0;
+      modelPosition = normalize(modelPosition);
+      vec3 modelNormal = normalize(vModelNormal);
+
+      // Calculate texture coorinates.
+      vec2 textureCoord;
+      textureCoord.s = -atan(-modelPosition.z, -modelPosition.x) / 2.0 / PI + 0.5;
+      textureCoord.t = 0.5 - 0.5 * modelPosition.y;
+
+      // Calculate tangent bases in world coordinates.
+      vec3 tangentBAxis = vec3(0.0,1.0,0.0);
+      tangentBAxis = normalize(tangentBAxis - dot(tangentBAxis, worldNormal) * worldNormal);
+      vec3 tangentTAxis = normalize(cross(tangentBAxis, worldNormal));
+
+      // Load normal map.
+      vec3 targetNormal = texture2D(uNormalMap, textureCoord).rgb;
+      // Hard-coded fine tuning
+      targetNormal.r = targetNormal.r / 129.0 * 127.5;
+      targetNormal.g = targetNormal.g / 128.0 * 127.5;
+      targetNormal = normalize(targetNormal * 2.0 - 1.0);
+
+      // Increase bumpiness
+      targetNormal.r = targetNormal.r * uBumpiness;
+      targetNormal.g = targetNormal.g * uBumpiness;
+      targetNormal = normalize(targetNormal);
+
+      // Calculate new normal after adding normal map
+      targetNormal = targetNormal.r * tangentTAxis - targetNormal.g * tangentBAxis + targetNormal.b * worldNormal;
+      targetNormal = normalize(targetNormal);
+
+      worldNormal = uNormalEnable * targetNormal + (1.0 - uNormalEnable) * worldNormal;
+
+      // Phong reflection model
+      vec3 normalizedLightDirection = normalize(uLightDirection);
+      vec3 vectorReflection = normalize( reflect(-normalizedLightDirection, worldNormal) );
+      vec3 vectorView = normalize( uViewOrigin - worldPosition );
+
+      float diffuseLightWeighting = max( dot(worldNormal, normalizedLightDirection), 0.0 );
+      float specularLightWeighting = pow( max( dot(vectorReflection, vectorView), 0.0), shininess );
+
+      // Sum up lighting and reflection parts
+      gl_FragColor = vec4(
+      ( uAmbientLight * vVertexColor)
+      + ((uDiffuseLight * vVertexColor) * diffuseLightWeighting)
+      + ( uSpecularLight * specularLightWeighting),
+      1.0 );
+      gl_FragColor += vec4(textureCube(uEnv, normalize(reflect(-vectorView, worldNormal))).rgb * uReflectionEnable,
+      0.0);
+      }`,
+    };
+  }
+}
---
+++ TeapotGlobe/js/startup.js
@@ -0,0 +1,176 @@
+/** Main GL entity */
+var gl;
+
+/** Shader prefixes */
+var shaderPrefix = [];
+/** Shader programs */
+var shaderPrograms = {};
+/** Shader initialization functions */
+var shaderInit = {};
+/** Buffer initialization functions */
+var bufferInit = {};
+/** Draw functions */
+var drawFunctions = {};
+/** Animate functions */
+var animateFunctions = {};
+/** Shaders */
+/** @type {Shaders} */
+const shaders = new Shaders();
+
+/** Enter point of the scripts for initialization of everything. */
+function startup() {
+  /** Create GL entity. */
+  var canvas = document.getElementById("myGLCanvas");
+  gl = createGLContext(canvas);
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
+  gl.enable(gl.DEPTH_TEST);
+
+  /** Initialize all scripts, shaders, and buffers. */
+  initAll();
+  setupShaders();
+  setupBuffers();
+
+  /** Start drawing! */
+  tick();
+}
+
+/** Render a frame. */
+function tick() {
+  requestAnimFrame(tick);
+
+  draw();
+  animate();
+}
+
+/** Create GL context.
+ *  @param {canvasElement} canvas
+ *  @return {glContext}
+ */
+function createGLContext(canvas) {
+  var names = ["webgl", "experimental-webgl"];
+  var context = null;
+  for (var i = 0; i < names.length; i++) {
+    try {
+      context = canvas.getContext(names[i]);
+      /** For debugging only */
+      // context = WebGLDebugUtils.makeDebugContext(canvas.getContext(names[i]));
+    } catch (e) {}
+    if (context) {
+      break;
+    }
+  }
+  if (context) {
+    context.viewportWidth = canvas.width;
+    context.viewportHeight = canvas.height;
+  } else {
+    alert("Failed to create WebGL context!");
+  }
+  return context;
+}
+
+/** Setup all shaders. */
+function setupShaders() {
+  for (prefix in shaders) {
+    setupOneShader(shaders[prefix]);
+    shaderInit[prefix]();
+  }
+}
+
+/** Setup one shader with specified identification prefix.
+ *  @param {Map} prefix
+ */
+function setupOneShader(prefix) {
+  /** Create new program. */
+  var oneShaderProgram = gl.createProgram();
+  /** Get shader codes. */
+  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  // var vertexShaderSource = document.getElementById(
+  //   prefix + "VertexShaderDOM"
+  // ).innerHTML;
+  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  // var fragmentShaderSource = document.getElementById(
+  //   prefix + "FragmentShaderDOM"
+  // ).innerHTML;
+
+  /** Compile and link program. */
+  gl.shaderSource(vertexShader, prefix.vertexShaderSource);
+  gl.compileShader(vertexShader);
+  gl.attachShader(oneShaderProgram, vertexShader);
+  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+    console.log(gl.getShaderInfoLog(vertexShader));
+  }
+
+  gl.shaderSource(fragmentShader, prefix.fragmentShaderSource);
+  gl.compileShader(fragmentShader);
+  gl.attachShader(oneShaderProgram, fragmentShader);
+  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+    console.log(gl.getShaderInfoLog(fragmentShader));
+  }
+
+  gl.linkProgram(oneShaderProgram);
+
+  if (!gl.getProgramParameter(oneShaderProgram, gl.LINK_STATUS)) {
+    console.log("Failed to setup shader:" + prefix.name);
+  }
+
+  shaderPrograms[prefix.name] = oneShaderProgram;
+}
+
+/** Setup all buffers. */
+function setupBuffers() {
+  for (prefix in shaders) {
+    bufferInit[prefix]();
+  }
+  // for (var i = 0; i < shaderPrefix.length; i++) {
+  //   bufferInit[shaderPrefix[i]]();
+  // }
+}
+
+/** Draw a frame by calling all draw functions. */
+function draw() {
+  gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  for (prefix in shaders) {
+    drawFunctions[prefix]();
+  }
+  // for (var i = 0; i < shaderPrefix.length; i++) {
+  //   drawFunctions[shaderPrefix[i]]();
+  // }
+}
+
+/** Animate by calling all animate functions. */
+function animate() {
+  /** Update physics and views. */
+  viewUpdateMatrix();
+
+  /** Call animate functions. */
+  for (prefix in shaders) {
+    animateFunctions[prefix]();
+  }
+  // for (var i = 0; i < shaderPrefix.length; i++) {
+  //   animateFunctions[shaderPrefix[i]]();
+  // }
+}
+
+/** Convert degree to rad.
+ *  @param {float} d degree
+ *  @return {float} r rad
+ */
+function degToRad(d) {
+  return (d * Math.PI) / 180;
+}
+
+/** Convert rad to degree.
+ *  @param {float} r rad
+ *  @return {float} d degree
+ */
+function radToDeg(r) {
+  return (r / Math.PI) * 180;
+}
+
+/** Initialize all scripts. */
+function initAll() {
+  viewInit();
+  skyboxInit();
+  teapotInit();
+}
