5a8aa5dc5e7761de232aebe476cbe8a3a7c0885d
vakshit <akshitv18@gmail.com>
2023-10-19 22:14:38 +0530

    complete assignment 4

--- assignment4/main.js
+++ assignment4/main.js
@@ -10,136 +10,148 @@ void main() {
 
 var fragmentShaderCode = `#version 300 es
 precision mediump float;
+
 in vec2 vTexCoord;
+
 out vec4 fragColor;
+
+uniform sampler2D backgroundSampler;
+uniform sampler2D foregroundSampler;
+
+uniform float background;
 uniform float contrast;
 uniform float brightness;
-uniform float grayScale;
+
+uniform float greyCheck;
 uniform float sepiaCheck;
-uniform float background;
-uniform float alpha;
-uniform sampler2D uSampler;
-uniform sampler2D newSampler;
-uniform float gradient;
-uniform float smoothf;
-uniform float sharpenf;
-uniform float laplacianf;
-void main(){
 
-    vec4 colorb = texture(uSampler,vTexCoord);
-    vec4 colorf = texture(newSampler,vTexCoord);
-    float af =  colorf.a;
+uniform float gradientCheck;
+uniform float smoothCheck;
+uniform float sharpenCheck;
+uniform float laplacianCheck;
 
+void grayScale(vec4 colorb) {
+  float gray = 0.2126f * colorb.r + 0.7152f * colorb.g + 0.0722f * colorb.b;
+  fragColor = vec4(gray, gray, gray, colorb.a);
+}
 
-    if(background == 1.0){
-        fragColor = colorb;
-        float gray = 0.2126*fragColor.r + 0.7152*fragColor.g + 0.0722*fragColor.b;
-        vec4 grayscalecolor = vec4(gray,gray,gray, fragColor.a);
-        if(grayScale == 1.0)
-        fragColor = grayscalecolor;
-        else fragColor = colorb;
-        
-        float sr = 0.393*fragColor.r + 0.769*fragColor.g + 0.189*fragColor.b;
-        float sg = 0.349*fragColor.r + 0.686*fragColor.g + 0.168*fragColor.b;
-        float sb = 0.272*fragColor.r + 0.534*fragColor.g + 0.131*fragColor.b;
-        vec4 sepia = vec4(sr,sg,sb,fragColor.a);
-        if(sepiaCheck == 1.0)
-        fragColor = sepia;
-        if(gradient == 1.0){
-            vec2 onePixel = vec2(1,1) / vec2(textureSize(uSampler, 0));
-            vec4 up = texture(uSampler, vTexCoord + 5.0*onePixel*vec2(0,-1));
-            vec4 down = texture(uSampler, vTexCoord + 5.0*onePixel*vec2(0,1));
-            vec4 right = texture(uSampler, vTexCoord + 5.0*onePixel*vec2(1,0));
-            vec4 left = texture(uSampler, vTexCoord + 5.0*onePixel*vec2(-1,0));
-            vec4 dy = (up-down);
-            vec4 dx = (right-left);
-            vec4 gradMag = sqrt(dx*dx+dy*dy);
-            if(gradMag.r<0.1 ||gradMag.g<0.1 ||gradMag.b<0.1 ) fragColor = vec4(0.0,0.0,0.0,1.0);
-        }
-        else if(smoothf == 1.0){
-            vec2 onePixel = vec2(1,1) / vec2(textureSize(uSampler, 0));
-            const float kernel[9] = float[9](
-                1.0, 1.0, 1.0,
-                1.0, 1.0, 1.0,
-                1.0, 1.0, 1.0
-            );
-            vec4 smoothedColor = colorb / 9.0;
-            int index=0;
-            for (int x = -1; x <= 1; x++) {
-                for (int y = -1; y <= 1; y++) {
-                    smoothedColor += texture(uSampler, vTexCoord + 5.0*onePixel * vec2(float(x), float(y))) * kernel[index];
-                    index++;
-                }
-            }
-            fragColor = vec4((smoothedColor/9.0).rgb,colorb.a);
-        }
-        else if(sharpenf == 1.0){
-            vec2 onePixel = vec2(1,1) / vec2(textureSize(uSampler, 0));
-            const float kernel[9] = float[9](
-                0.0, -1.0, 0.0,
-                -1.0, 5.0, -1.0,
-                0.0, -1.0, 0.0
-            );
-            vec4 smoothedColor = colorb/9.0;
-            int index=0;
-            for (int x = -1; x <= 1; x++) {
-                for (int y = -1; y <= 1; y++) {
-                    smoothedColor += texture(uSampler, vTexCoord + 5.0*onePixel * vec2(float(x), float(y))) * kernel[index];
-                    index++;
-                }
-            }
-            fragColor = vec4((smoothedColor).rgb,colorb.a);
-        }
-        else if(laplacianf == 1.0){
-            vec2 onePixel = vec2(1,1) / vec2(textureSize(uSampler, 0));
-            const float kernel[9] = float[9](
-                0.0, -1.0, 0.0,
-                -1.0, 4.0, -1.0,
-                0.0, -1.0, 0.0
-            );
-            vec4 smoothedColor = colorb/9.0;
-            int index=0;
-            for (int x = -1; x <= 1; x++) {
-                for (int y = -1; y <= 1; y++) {
-                    smoothedColor += texture(uSampler, vTexCoord + 3.0*onePixel * vec2(float(x), float(y))) * kernel[index];
-                    index++;
-                }
-            }
-            fragColor = vec4((smoothedColor).rgb,colorb.a);
-        }
+void sepia(vec4 color) {
+  float sr = 0.393f * color.r + 0.769f * color.g + 0.189f * color.b;
+  float sg = 0.349f * color.r + 0.686f * color.g + 0.168f * color.b;
+  float sb = 0.272f * color.r + 0.534f * color.g + 0.131f * color.b;
+  fragColor = vec4(sr, sg, sb, color.a);
+}
+
+void gradient() {
+  vec2 onePixel = vec2(1, 1) / vec2(textureSize(backgroundSampler, 0));
+  vec4 up = texture(backgroundSampler, vTexCoord + 5.0f * onePixel * vec2(0, -1));
+  vec4 down = texture(backgroundSampler, vTexCoord + 5.0f * onePixel * vec2(0, 1));
+  vec4 right = texture(backgroundSampler, vTexCoord + 5.0f * onePixel * vec2(1, 0));
+  vec4 left = texture(backgroundSampler, vTexCoord + 5.0f * onePixel * vec2(-1, 0));
+  vec4 dy = (up - down);
+  vec4 dx = (right - left);
+  vec4 gradMag = sqrt(dx * dx + dy * dy);
+  if(gradMag.r < 0.1f || gradMag.g < 0.1f || gradMag.b < 0.1f)
+    fragColor = vec4(0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+void smoothen(vec4 color) {
+  vec2 onePixel = vec2(1, 1) / vec2(textureSize(backgroundSampler, 0));
+  const float kernel[9] = float[9](1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f);
+  vec4 smoothedColor = color / 9.0f;
+  int index = 0;
+  for(int x = -1; x <= 1; x++) {
+    for(int y = -1; y <= 1; y++) {
+      smoothedColor += texture(backgroundSampler, vTexCoord + 5.0f * onePixel * vec2(float(x), float(y))) * kernel[index];
+      index++;
     }
-    else if(alpha == 1.0){
-        vec4 blend = af*colorf + (1.0-af)*colorb;
-        fragColor = blend;
-        float gray = 0.2126*fragColor.r + 0.7152*fragColor.g + 0.0722*fragColor.b;
-        vec4 grayscalecolor = vec4(gray,gray,gray, fragColor.a);
-        if(grayScale == 1.0)
-        fragColor = grayscalecolor;
-        
-        float sr = 0.393*fragColor.r + 0.769*fragColor.g + 0.189*fragColor.b;
-        float sg = 0.349*fragColor.r + 0.686*fragColor.g + 0.168*fragColor.b;
-        float sb = 0.272*fragColor.r + 0.534*fragColor.g + 0.131*fragColor.b;
-        vec4 sepia = vec4(sr,sg,sb,fragColor.a);
-        if(sepiaCheck == 1.0)
-        fragColor = sepia;
+  }
+  fragColor = vec4((smoothedColor / 9.0f).rgb, color.a);
+}
+
+void sharpen(vec4 color) {
+  vec2 onePixel = vec2(1, 1) / vec2(textureSize(backgroundSampler, 0));
+  const float kernel[9] = float[9](0.0f, -1.0f, 0.0f, -1.0f, 5.0f, -1.0f, 0.0f, -1.0f, 0.0f);
+  vec4 smoothedColor = color / 9.0f;
+  int index = 0;
+  for(int x = -1; x <= 1; x++) {
+    for(int y = -1; y <= 1; y++) {
+      smoothedColor += texture(backgroundSampler, vTexCoord + 5.0f * onePixel * vec2(float(x), float(y))) * kernel[index];
+      index++;
     }
-    
-    fragColor = vec4(0.5+ (contrast+ 1.0) * (fragColor.rgb - 0.5) , fragColor.a);
-    fragColor = clamp(vec4(fragColor.rgb + brightness,fragColor.a), 0.0,  1.0);
-    
+  }
+  fragColor = vec4((smoothedColor).rgb, color.a);
+}
+
+void laplacian(vec4 color) {
+  vec2 onePixel = vec2(1, 1) / vec2(textureSize(backgroundSampler, 0));
+  const float kernel[9] = float[9](0.0f, -1.0f, 0.0f, -1.0f, 4.0f, -1.0f, 0.0f, -1.0f, 0.0f);
+  vec4 smoothedColor = color / 9.0f;
+  int index = 0;
+  for(int x = -1; x <= 1; x++) {
+    for(int y = -1; y <= 1; y++) {
+      smoothedColor += texture(backgroundSampler, vTexCoord + 3.0f * onePixel * vec2(float(x), float(y))) * kernel[index];
+      index++;
+    }
+  }
+  fragColor = vec4((smoothedColor).rgb, color.a);
+}
+
+void main() {
+  vec4 colorb = texture(backgroundSampler, vTexCoord);
+  vec4 colorf = texture(foregroundSampler, vTexCoord);
+
+  float af = colorf.a;
+
+  if(background == 0.0f) {
+    fragColor = colorb;
+
+    if(greyCheck == 1.0f) {
+      grayScale(colorb);
+    }
+
+    if(sepiaCheck == 1.0f) {
+      sepia(colorb);
+    }
+
+  } else if(background == 1.0f) {
+    vec4 blend = af * colorf + (1.0f - af) * colorb;
+    fragColor = blend;
+    if(greyCheck == 1.0f)
+      grayScale(blend);
+    else if(sepiaCheck == 1.0f)
+      sepia(blend);
+  }
+  if(gradientCheck == 1.0f) {
+    gradient();
+  } else if(smoothCheck == 1.0f) {
+    smoothen(colorb);
+  } else if(sharpenCheck == 1.0f) {
+    sharpen(colorb);
+  } else if(laplacianCheck == 1.0f) {
+    laplacian(colorb);
+  }
+
+  fragColor = vec4(0.5f + (contrast + 1.0f) * (fragColor.rgb - 0.5f), fragColor.a);
+  fragColor = clamp(vec4(fragColor.rgb + brightness, fragColor.a), 0.0f, 1.0f);
+
 }
 `;
-var contrast;
-var brightness;
-var grayScale;
-var sepiaCheck;
 var background;
-var aplhacheck;
-var gradient;
-var smooth;
-var imageInput;
-var loadedImage;
-var imageUrl;
+
+var contrast = 0.0;
+var brightness = 0.0;
+
+var effects = {
+  grayscale: 0.0,
+  sepia: 0.0,
+
+  smooth: 0.0,
+  gradient: 0.0,
+  sharpen: 0.0,
+  laplacian: 0.0,
+};
+
 var canvas;
 var gl;
 var shaderProgram;
@@ -147,21 +159,21 @@ var positionBuffer;
 var positionAttribLocation;
 var texCoordAttribLocation;
 var texCoordBuffer;
-var texture;
 var sqVertexIndexBuffer;
-var newTexture;
 var contrastLocation;
 var grayCheckboxLocation;
 var sepiaCheckboxLocation;
 var backgroundLocation;
-var alphaLocation;
 var gradientLocation;
 var smoothLocation;
 var sharpenLocation;
 var laplacianLocation;
+
 function initGL(canvas) {
   try {
-    gl = canvas.getContext("webgl2"); // the graphics webgl2 context
+    gl = canvas.getContext("webgl2", {
+      preserveDrawingBuffer: true,
+    }); // the graphics webgl2 context
     gl.viewportWidth = canvas.width; // the width of the canvas
     gl.viewportHeight = canvas.height; // the height
   } catch (e) {}
@@ -169,21 +181,10 @@ function initGL(canvas) {
     alert("WebGL initialization failed");
   }
 }
-function vertexShaderSetup(vertexShaderCode) {
-  shader = gl.createShader(gl.VERTEX_SHADER);
-  gl.shaderSource(shader, vertexShaderCode);
-  gl.compileShader(shader);
-  // Error check whether the shader is compiled correctly
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    alert(gl.getShaderInfoLog(shader));
-    return null;
-  }
-  return shader;
-}
 
-function fragmentShaderSetup(fragShaderCode) {
-  shader = gl.createShader(gl.FRAGMENT_SHADER);
-  gl.shaderSource(shader, fragShaderCode);
+function shaderSetup(shaderCode, type) {
+  shader = gl.createShader(type);
+  gl.shaderSource(shader, shaderCode);
   gl.compileShader(shader);
   // Error check whether the shader is compiled correctly
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
@@ -195,9 +196,8 @@ function fragmentShaderSetup(fragShaderCode) {
 
 function initShaders() {
   shaderProgram = gl.createProgram();
-
-  var vertexShader = vertexShaderSetup(vertexShaderCode);
-  var fragmentShader = fragmentShaderSetup(fragmentShaderCode);
+  var vertexShader = shaderSetup(vertexShaderCode, gl.VERTEX_SHADER);
+  var fragmentShader = shaderSetup(fragmentShaderCode, gl.FRAGMENT_SHADER);
 
   // attach the shaders
   gl.attachShader(shaderProgram, vertexShader);
@@ -222,14 +222,13 @@ const loadUniforms = () => {
   texCoordAttribLocation = gl.getAttribLocation(shaderProgram, "aTexCoord");
   contrastLocation = gl.getUniformLocation(shaderProgram, "contrast");
   brightnessLocation = gl.getUniformLocation(shaderProgram, "brightness");
-  grayCheckboxLocation = gl.getUniformLocation(shaderProgram, "grayScale");
+  grayCheckboxLocation = gl.getUniformLocation(shaderProgram, "greyCheck");
   sepiaCheckboxLocation = gl.getUniformLocation(shaderProgram, "sepiaCheck");
   backgroundLocation = gl.getUniformLocation(shaderProgram, "background");
-  alphaLocation = gl.getUniformLocation(shaderProgram, "alpha");
-  gradientLocation = gl.getUniformLocation(shaderProgram, "gradient");
-  smoothLocation = gl.getUniformLocation(shaderProgram, "smoothf");
-  sharpenLocation = gl.getUniformLocation(shaderProgram, "sharpenf");
-  laplacianLocation = gl.getUniformLocation(shaderProgram, "laplacianf");
+  gradientLocation = gl.getUniformLocation(shaderProgram, "gradientCheck");
+  smoothLocation = gl.getUniformLocation(shaderProgram, "smoothCheck");
+  sharpenLocation = gl.getUniformLocation(shaderProgram, "sharpenCheck");
+  laplacianLocation = gl.getUniformLocation(shaderProgram, "laplacianCheck");
 
   const vertexData = new Float32Array([
     1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0,
@@ -252,8 +251,7 @@ const loadUniforms = () => {
 };
 
 const drawScene = async () => {
-  gl.clearColor(0.9, 0.9, 0.9, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  clearCanvas();
 
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   gl.vertexAttribPointer(positionAttribLocation, 2, gl.FLOAT, false, 0, 0);
@@ -264,16 +262,15 @@ const drawScene = async () => {
 
   gl.uniform1f(contrastLocation, contrast);
   gl.uniform1f(brightnessLocation, brightness);
-  gl.uniform1f(grayCheckboxLocation, grayScale);
-  gl.uniform1f(sepiaCheckboxLocation, sepiaCheck);
+  gl.uniform1f(grayCheckboxLocation, effects.grayscale);
+  gl.uniform1f(sepiaCheckboxLocation, effects.sepia);
   gl.uniform1f(backgroundLocation, background);
-  gl.uniform1f(alphaLocation, aplhacheck);
-  gl.uniform1f(gradientLocation, gradient);
-  gl.uniform1f(smoothLocation, smooth);
-  gl.uniform1f(sharpenLocation, sharpen);
-  gl.uniform1f(laplacianLocation, laplacian);
-  gl.uniform1i(gl.getUniformLocation(shaderProgram, "uSampler"), 0);
-  gl.uniform1i(gl.getUniformLocation(shaderProgram, "newSampler"), 1);
+  gl.uniform1f(gradientLocation, effects.gradient);
+  gl.uniform1f(smoothLocation, effects.smooth);
+  gl.uniform1f(sharpenLocation, effects.sharpen);
+  gl.uniform1f(laplacianLocation, effects.laplacian);
+  gl.uniform1i(gl.getUniformLocation(shaderProgram, "backgroundSampler"), 0);
+  gl.uniform1i(gl.getUniformLocation(shaderProgram, "foregroundSampler"), 1);
 
   gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
 };
@@ -283,6 +280,6 @@ async function webGLStart() {
   initGL(canvas);
   gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
   shaderProgram = initShaders();
-  addEventListeners();
   loadUniforms();
+  clearCanvas();
 }
--- assignment4/utils.js
+++ assignment4/utils.js
@@ -15,127 +15,6 @@ const loadImage = (e, number) => {
   }
 };
 
-const addEventListeners = () => {
-  contrast = 0.0;
-  positionSlider = document.getElementById("contrast");
-  positionSlider.addEventListener("input", () => {
-    contrast = positionSlider.value;
-    drawScene();
-  });
-
-  brightness = 0.0;
-  brightnessSlider = document.getElementById("brightness");
-  brightnessSlider.addEventListener("input", () => {
-    brightness = brightnessSlider.value;
-    drawScene();
-  });
-
-  grayScale = 0.0;
-  grayCheckbox = document.getElementById("grayScale");
-  grayCheckbox.addEventListener("change", () => {
-    if (grayCheckbox.checked) {
-      grayScale = 1.0;
-    } else {
-      grayScale = 0.0;
-    }
-    drawScene();
-  });
-
-  sepiaCheck = 0.0;
-  sepiaCheckbox = document.getElementById("sepiaCheck");
-  sepiaCheckbox.addEventListener("change", () => {
-    if (sepiaCheckbox.checked) {
-      sepiaCheck = 1.0;
-    } else {
-      sepiaCheck = 0.0;
-    }
-    drawScene();
-  });
-
-  background = 0.0;
-  backgroundCheckbox = document.getElementById("backgroundCheck");
-  backgroundCheckbox.addEventListener("change", () => {
-    if (backgroundCheckbox.checked) {
-      background = 1.0;
-      aplhacheck = 0.0;
-      aplhacheckbox.checked = false;
-    } else {
-      background = 0.0;
-    }
-    drawScene();
-  });
-
-  aplhacheck = 0.0;
-  aplhacheckbox = document.getElementById("alphaCheck");
-  aplhacheckbox.addEventListener("change", () => {
-    if (aplhacheckbox.checked) {
-      aplhacheck = 1.0;
-      background = 0.0;
-      backgroundCheckbox.checked = false;
-    } else {
-      aplhacheck = 0.0;
-    }
-    drawScene();
-  });
-
-  gradient = 0.0;
-  gradcheckbox = document.getElementById("gradient");
-  gradcheckbox.addEventListener("change", () => {
-    if (gradcheckbox.checked) {
-      gradient = 1.0;
-      laplacian = 0.0;
-      laplacianbox.checked = false;
-      sharpenBox.checked = false;
-      smoothbox.checked = false;
-    } else {
-      gradient = 0.0;
-    }
-    drawScene();
-  });
-
-  smooth = 0.0;
-  smoothbox = document.getElementById("smooth");
-  smoothbox.addEventListener("change", () => {
-    if (smoothbox.checked) {
-      smooth = 1.0;
-      gradcheckbox.checked = false;
-      laplacianbox.checked = false;
-      sharpenBox.checked = false;
-    } else {
-      smooth = 0.0;
-    }
-    drawScene();
-  });
-
-  sharpen = 0.0;
-  sharpenBox = document.getElementById("Sharpen");
-  sharpenBox.addEventListener("change", () => {
-    if (sharpenBox.checked) {
-      sharpen = 1.0;
-      gradcheckbox.checked = false;
-      smoothbox.checked = false;
-      laplacianbox.checked = false;
-    } else {
-      sharpen = 0.0;
-    }
-    drawScene();
-  });
-
-  laplacian = 0.0;
-  laplacianbox = document.getElementById("laplacian");
-  laplacianbox.addEventListener("change", () => {
-    if (laplacianbox.checked) {
-      laplacian = 1.0;
-      gradcheckbox.checked = false;
-      smoothbox.checked = false;
-      sharpenBox.checked = false;
-    } else {
-      laplacian = 0.0;
-    }
-    drawScene();
-  });
-};
-
 const loadTexture = async (number, image) => {
   texture = gl.createTexture();
   gl.activeTexture(gl.TEXTURE0 + number);
@@ -154,4 +33,34 @@ const loadTexture = async (number, image) => {
   gl.generateMipmap(gl.TEXTURE_2D);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+};
+
+const setEffects = (effect) => {
+  for (const key in effects) {
+    effects[key] = 0.0;
+  }
+  if (effect) {
+    if (["smooth", "gradient", "sharpen", "laplacian"].includes(effect)) {
+      imageModes = document.getElementsByName("imageMode");
+      imageModes[0].checked = true;
+      imageModes[1].checked = false;
+      background = 0.0;
+    }
+    effects[effect] = 1.0;
+  } else {
+    document.getElementById("none").checked = true;
+  }
+};
+
+const clearCanvas = () => {
+  gl.clearColor(0.99, 0.99, 0.99, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+};
+
+const downloadImage = () => {
+  const image = document.getElementById("canvas");
+  const link = document.createElement("a");
+  link.download = "image.png";
+  link.href = image.toDataURL("image/png");
+  link.click();
 };
