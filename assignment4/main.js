29ad46182d609580ca6967c205c8e2790ccc8911
vakshit <akshitv18@gmail.com>
2023-10-19 22:31:06 +0530

    add font families

--- assignment4/main.js
+++ assignment4/main.js
@@ -30,24 +30,25 @@ uniform float smoothCheck;
 uniform float sharpenCheck;
 uniform float laplacianCheck;
 
-void grayScale(vec4 colorb) {
-  float gray = 0.2126f * colorb.r + 0.7152f * colorb.g + 0.0722f * colorb.b;
-  fragColor = vec4(gray, gray, gray, colorb.a);
+void grayScale(vec4 color) {
+  float gray = 0.2126f * color.r + 0.7152f * color.g + 0.0722f * color.b;
+  fragColor = vec4(gray, gray, gray, color.a);
 }
 
 void sepia(vec4 color) {
-  float sr = 0.393f * color.r + 0.769f * color.g + 0.189f * color.b;
-  float sg = 0.349f * color.r + 0.686f * color.g + 0.168f * color.b;
-  float sb = 0.272f * color.r + 0.534f * color.g + 0.131f * color.b;
-  fragColor = vec4(sr, sg, sb, color.a);
+
+  fragColor.r = dot(color.rgb, vec3(0.393f, 0.769f, 0.189f));
+  fragColor.g = dot(color.rgb, vec3(0.349f, 0.686f, 0.168f));
+  fragColor.b = dot(color.rgb, vec3(0.272f, 0.534f, 0.131f));
+  fragColor.a = color.a;
 }
 
 void gradient() {
-  vec2 onePixel = vec2(1, 1) / vec2(textureSize(backgroundSampler, 0));
-  vec4 up = texture(backgroundSampler, vTexCoord + 5.0f * onePixel * vec2(0, -1));
-  vec4 down = texture(backgroundSampler, vTexCoord + 5.0f * onePixel * vec2(0, 1));
-  vec4 right = texture(backgroundSampler, vTexCoord + 5.0f * onePixel * vec2(1, 0));
-  vec4 left = texture(backgroundSampler, vTexCoord + 5.0f * onePixel * vec2(-1, 0));
+  vec2 pixel = vec2(1, 1) / vec2(textureSize(backgroundSampler, 0));
+  vec4 up = texture(backgroundSampler, vTexCoord + 5.0f * pixel * vec2(0, -1));
+  vec4 down = texture(backgroundSampler, vTexCoord + 5.0f * pixel * vec2(0, 1));
+  vec4 right = texture(backgroundSampler, vTexCoord + 5.0f * pixel * vec2(1, 0));
+  vec4 left = texture(backgroundSampler, vTexCoord + 5.0f * pixel * vec2(-1, 0));
   vec4 dy = (up - down);
   vec4 dx = (right - left);
   vec4 gradMag = sqrt(dx * dx + dy * dy);
@@ -56,13 +57,13 @@ void gradient() {
 }
 
 void smoothen(vec4 color) {
-  vec2 onePixel = vec2(1, 1) / vec2(textureSize(backgroundSampler, 0));
+  vec2 pixel = vec2(1, 1) / vec2(textureSize(backgroundSampler, 0));
   const float kernel[9] = float[9](1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f);
   vec4 smoothedColor = color / 9.0f;
   int index = 0;
   for(int x = -1; x <= 1; x++) {
     for(int y = -1; y <= 1; y++) {
-      smoothedColor += texture(backgroundSampler, vTexCoord + 5.0f * onePixel * vec2(float(x), float(y))) * kernel[index];
+      smoothedColor += texture(backgroundSampler, vTexCoord + 5.0f * pixel * vec2(float(x), float(y))) * kernel[index];
       index++;
     }
   }
@@ -70,13 +71,13 @@ void smoothen(vec4 color) {
 }
 
 void sharpen(vec4 color) {
-  vec2 onePixel = vec2(1, 1) / vec2(textureSize(backgroundSampler, 0));
+  vec2 pixel = vec2(1, 1) / vec2(textureSize(backgroundSampler, 0));
   const float kernel[9] = float[9](0.0f, -1.0f, 0.0f, -1.0f, 5.0f, -1.0f, 0.0f, -1.0f, 0.0f);
   vec4 smoothedColor = color / 9.0f;
   int index = 0;
   for(int x = -1; x <= 1; x++) {
     for(int y = -1; y <= 1; y++) {
-      smoothedColor += texture(backgroundSampler, vTexCoord + 5.0f * onePixel * vec2(float(x), float(y))) * kernel[index];
+      smoothedColor += texture(backgroundSampler, vTexCoord + 5.0f * pixel * vec2(float(x), float(y))) * kernel[index];
       index++;
     }
   }
@@ -84,13 +85,13 @@ void sharpen(vec4 color) {
 }
 
 void laplacian(vec4 color) {
-  vec2 onePixel = vec2(1, 1) / vec2(textureSize(backgroundSampler, 0));
+  vec2 pixel = vec2(1, 1) / vec2(textureSize(backgroundSampler, 0));
   const float kernel[9] = float[9](0.0f, -1.0f, 0.0f, -1.0f, 4.0f, -1.0f, 0.0f, -1.0f, 0.0f);
   vec4 smoothedColor = color / 9.0f;
   int index = 0;
   for(int x = -1; x <= 1; x++) {
     for(int y = -1; y <= 1; y++) {
-      smoothedColor += texture(backgroundSampler, vTexCoord + 3.0f * onePixel * vec2(float(x), float(y))) * kernel[index];
+      smoothedColor += texture(backgroundSampler, vTexCoord + 3.0f * pixel * vec2(float(x), float(y))) * kernel[index];
       index++;
     }
   }
@@ -100,8 +101,6 @@ void laplacian(vec4 color) {
 void main() {
   vec4 colorb = texture(backgroundSampler, vTexCoord);
   vec4 colorf = texture(foregroundSampler, vTexCoord);
-
-  float af = colorf.a;
 
   if(background == 0.0f) {
     fragColor = colorb;
@@ -115,13 +114,14 @@ void main() {
     }
 
   } else if(background == 1.0f) {
-    vec4 blend = af * colorf + (1.0f - af) * colorb;
+    vec4 blend = colorf.a * colorf + (1.0f - colorf.a) * colorb;
     fragColor = blend;
     if(greyCheck == 1.0f)
       grayScale(blend);
     else if(sepiaCheck == 1.0f)
       sepia(blend);
   }
+  // check for background filters
   if(gradientCheck == 1.0f) {
     gradient();
   } else if(smoothCheck == 1.0f) {
@@ -131,10 +131,9 @@ void main() {
   } else if(laplacianCheck == 1.0f) {
     laplacian(colorb);
   }
-
+  // contrast and brightness
   fragColor = vec4(0.5f + (contrast + 1.0f) * (fragColor.rgb - 0.5f), fragColor.a);
   fragColor = clamp(vec4(fragColor.rgb + brightness, fragColor.a), 0.0f, 1.0f);
-
 }
 `;
 var background;
@@ -160,14 +159,6 @@ var positionAttribLocation;
 var texCoordAttribLocation;
 var texCoordBuffer;
 var sqVertexIndexBuffer;
-var contrastLocation;
-var grayCheckboxLocation;
-var sepiaCheckboxLocation;
-var backgroundLocation;
-var gradientLocation;
-var smoothLocation;
-var sharpenLocation;
-var laplacianLocation;
 
 function initGL(canvas) {
   try {
@@ -220,15 +211,6 @@ function initShaders() {
 const loadUniforms = () => {
   positionAttribLocation = gl.getAttribLocation(shaderProgram, "aPosition");
   texCoordAttribLocation = gl.getAttribLocation(shaderProgram, "aTexCoord");
-  contrastLocation = gl.getUniformLocation(shaderProgram, "contrast");
-  brightnessLocation = gl.getUniformLocation(shaderProgram, "brightness");
-  grayCheckboxLocation = gl.getUniformLocation(shaderProgram, "greyCheck");
-  sepiaCheckboxLocation = gl.getUniformLocation(shaderProgram, "sepiaCheck");
-  backgroundLocation = gl.getUniformLocation(shaderProgram, "background");
-  gradientLocation = gl.getUniformLocation(shaderProgram, "gradientCheck");
-  smoothLocation = gl.getUniformLocation(shaderProgram, "smoothCheck");
-  sharpenLocation = gl.getUniformLocation(shaderProgram, "sharpenCheck");
-  laplacianLocation = gl.getUniformLocation(shaderProgram, "laplacianCheck");
 
   const vertexData = new Float32Array([
     1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0,
@@ -260,15 +242,33 @@ const drawScene = async () => {
   gl.vertexAttribPointer(texCoordAttribLocation, 2, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(texCoordAttribLocation);
 
-  gl.uniform1f(contrastLocation, contrast);
-  gl.uniform1f(brightnessLocation, brightness);
-  gl.uniform1f(grayCheckboxLocation, effects.grayscale);
-  gl.uniform1f(sepiaCheckboxLocation, effects.sepia);
-  gl.uniform1f(backgroundLocation, background);
-  gl.uniform1f(gradientLocation, effects.gradient);
-  gl.uniform1f(smoothLocation, effects.smooth);
-  gl.uniform1f(sharpenLocation, effects.sharpen);
-  gl.uniform1f(laplacianLocation, effects.laplacian);
+  gl.uniform1f(gl.getUniformLocation(shaderProgram, "contrast"), contrast);
+  gl.uniform1f(gl.getUniformLocation(shaderProgram, "brightness"), brightness);
+  gl.uniform1f(
+    gl.getUniformLocation(shaderProgram, "greyCheck"),
+    effects.grayscale
+  );
+  gl.uniform1f(
+    gl.getUniformLocation(shaderProgram, "sepiaCheck"),
+    effects.sepia
+  );
+  gl.uniform1f(gl.getUniformLocation(shaderProgram, "background"), background);
+  gl.uniform1f(
+    gl.getUniformLocation(shaderProgram, "gradientCheck"),
+    effects.gradient
+  );
+  gl.uniform1f(
+    gl.getUniformLocation(shaderProgram, "smoothCheck"),
+    effects.smooth
+  );
+  gl.uniform1f(
+    gl.getUniformLocation(shaderProgram, "sharpenCheck"),
+    effects.sharpen
+  );
+  gl.uniform1f(
+    gl.getUniformLocation(shaderProgram, "laplacianCheck"),
+    effects.laplacian
+  );
   gl.uniform1i(gl.getUniformLocation(shaderProgram, "backgroundSampler"), 0);
   gl.uniform1i(gl.getUniformLocation(shaderProgram, "foregroundSampler"), 1);
 
